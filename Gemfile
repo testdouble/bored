e80a9e788e7d2f5a22c20e54a1770ef933a648ba
Justin Searls <searls@gmail.com>
2021-12-02 08:50:13 -0500

    simplify test assertions, add a test that checks two parameters

--- Gemfile
+++ Gemfile
@@ -1,12 +1,9 @@
-# frozen_string_literal: true
-
 source "https://rubygems.org"
 
 # Specify your gem's dependencies in bored.gemspec
 gemspec
 
-gem "rake", "~> 13.0"
-
-gem "minitest", "~> 5.0"
-
-gem "standard", "~> 1.3"
+gem "rake"
+gem "minitest"
+gem "standard"
+gem "pry"
--- test/test_bored.rb
+++ test/test_bored.rb
@@ -25,12 +25,9 @@ class TestBored < Minitest::Test
   def test_that_it_gives_you_an_activity_with_a_specific_key
     activity = Bored.now(key: "1878070")
 
-    assert_kind_of String, activity.description
     assert_equal "Volunteer at your local food pantry", activity.description
-    assert_includes([
-      :education, :recreational, :social, :diy, :charity,
-      :cooking, :relaxation, :music, :busywork
-    ], activity.type)
+    assert_equal 1, activity.participants
+    assert_equal :charity, activity.type
   end
 
   def test_that_it_raises_an_error_with_a_bad_key
@@ -43,5 +40,17 @@ class TestBored < Minitest::Test
     activity = Bored.now(participants: 3)
 
     assert_equal 3, activity.participants
+  end
+
+  def test_that_it_can_search_on_two_criteria
+    activity = Bored.now(key: "1878070", participants: 1)
+
+    assert_equal "Volunteer at your local food pantry", activity.description
+  end
+
+  def test_that_it_raises_when_two_criteria_yield_no_results
+    assert_raises Bored::Error do
+      Bored.now(key: "1878070", participants: 2)
+    end
   end
 end
